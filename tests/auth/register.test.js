5e4fa89eb1a52ed8e6875ebb5a349a94ba333b45
Krishna Pandita <kpandita68@gmail.com>
2026-07-21 17:25:26 +0530

    git commit -m "test: add login tests"

--- tests/auth/register.test.js
+++ tests/auth/register.test.js
@@ -1,5 +1,17 @@
-const request = require("supertest");
-const app = require("../../src/app"); // your Express app
+import request from "supertest";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+import app from "../../src/app.js";
+import connectDB from "../../database/db.js"; // adjust path if needed
+
+beforeAll(async () => {
+  await connectDB();
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
 
 describe("POST /api/auth/register", () => {
   it("should register a new user", async () => {
@@ -8,9 +20,42 @@ describe("POST /api/auth/register", () => {
       .send({
         name: "Krishna",
         email: "krishna@example.com",
-        password: "password123"
+        password: "password123",
       });
 
     expect(response.statusCode).toBe(201);
   });
+});
+
+
+describe("User Login", () => {
+
+  it("should login an existing user", async () => {
+
+    await request(app)
+      .post("/api/auth/register")
+      .send({
+        name: "Krishna",
+        email: "krishna@gmail.com",
+        password: "123456"
+      });
+
+
+    const response = await request(app)
+      .post("/api/auth/login")
+      .send({
+        email: "krishna@gmail.com",
+        password: "123456"
+      });
+
+
+    expect(response.statusCode).toBe(200);
+
+    expect(response.body).toHaveProperty("token");
+
+    expect(response.body.message)
+      .toBe("Login successful");
+
+  });
+
 });
