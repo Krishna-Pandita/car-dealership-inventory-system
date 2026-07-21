311695c435d0d52dd53dd80504ea9b208cabe150
Krishna Pandita <kpandita68@gmail.com>
2026-07-21 17:38:55 +0530

    test: add vehicle tests

--- tests/auth/register.test.js
+++ tests/auth/register.test.js
@@ -59,3 +59,31 @@ describe("User Login", () => {
   });
 
 });
+
+
+
+
+describe("POST /api/vehicles", () => {
+  it("should add a new vehicle", async () => {
+    const response = await request(app)
+      .post("/api/vehicles")
+      .send({
+        make: "Toyota",
+        model: "Fortuner",
+        category: "SUV",
+        price: 4500000,
+        quantity: 5,
+      });
+
+    expect(response.statusCode).toBe(201);
+
+    expect(response.body.message).toBe("Vehicle added successfully");
+
+    expect(response.body.vehicle).toHaveProperty("_id");
+    expect(response.body.vehicle.make).toBe("Toyota");
+    expect(response.body.vehicle.model).toBe("Fortuner");
+    expect(response.body.vehicle.category).toBe("SUV");
+    expect(response.body.vehicle.price).toBe(4500000);
+    expect(response.body.vehicle.quantity).toBe(5);
+  });
+});
